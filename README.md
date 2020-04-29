1fc83ba0e0c5ce077c2b82b9a07ede00af319158
diana <louxusian111@gmail.com>
2020-04-29 18:45:19 +0800

    init project

---
+++ README.md
@@ -0,0 +1,13 @@
+# hands 
+A frontend project for hands Taiwan
+> 推手計劃網站前端切版
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# build for production with minification
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        "style": "./src/scss/styles.scss"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./styles.css"
+        })
+    ]
+};
